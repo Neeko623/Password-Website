a0a530d35f73db961f3648334bcd15e66aa6a7aa
Neeko Tang <neeko_tvxq@hotmail.com>
2020-12-19 15:35:47 -0500

    upload files

---
+++ README.md
@@ -0,0 +1,14 @@
+# Password Generator
+A website that can use to generate a random password. 
+It is a reponsive and functional application, easy to navigate.
+## Code Style
+HTML
+CSS
+JAVA SCRIPT
+## Screenshots
+![Screeshot](./Password-Generator.png)
+![Screeshot](./Password-Generator (1).png)
+
+# Link to Webpage
+
+[Portfolio Website]()
--- script.js
+++ script.js
@@ -1,4 +1,193 @@
-// Assignment code here
+// Array of 4 different characters
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// First function to prompt in the browser
+function getPasswordOptions() {
+  // This is a Variable
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain?')
+  );
+
+  // Conditional statements a length of at least 8 characters and no more than 128 characters
+  if (isNaN(length) === true) {
+    alert('Password length must be provided as a number');
+    return;
+  }
+  if (length < 8) {
+    alert('Password length must be at least 8 characters');
+    return;
+  }
+  if (length > 128) {
+    alert('Password length must less than 129 characters');
+    return;
+  }
+
+// These are variables to confrim character types to include in the password
+  var hasSpecialCharacters = confirm(
+    'Click OK to confirm including special characters.'
+  );
+  var hasNumericCharacters = confirm(
+    'Click OK to confirm including numeric characters.'
+  );
+  var hasLowerCasedCharacters = confirm(
+    'Click OK to confirm including lowercase characters.'
+  );
+  var hasUpperCasedCharacters = confirm(
+    'Click OK to confirm including uppercase characters.'
+  );
+
+  // Conditional statement to user input should be validated and at least one character type should be selected
+  if (
+    hasSpecialCharacters === false &&
+    hasNumericCharacters === false &&
+    hasLowerCasedCharacters === false &&
+    hasUpperCasedCharacters === false
+  ) {
+    alert('Must select at least one character type');
+    return;
+  }
+
+  // Object to store user input
+  var passwordOptions = {
+    length: length,
+    hasSpecialCharacters: hasSpecialCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasLowerCasedCharacters: hasLowerCasedCharacters,
+    hasUpperCasedCharacters: hasUpperCasedCharacters
+  };
+
+  return passwordOptions;
+}
+
+// Second function that generated the password from the array
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
+
+// Third Function to generate password with user input
+function generatePassword() {
+  var options = getPasswordOptions();
+  var result = [];
+
+  var possibleCharacters = [];
+  var guaranteedCharacters = [];
+
+  // Conditional statement for 4 types of characters
+  if (options.hasSpecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteedCharacters.push(getRandom(specialCharacters));
+  }
+  if (options.hasNumericCharacters) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+    guaranteedCharacters.push(getRandom(numericCharacters));
+  }
+  if (options.hasLowerCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+    guaranteedCharacters.push(getRandom(lowerCasedCharacters));
+  }
+  if (options.hasUpperCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+    guaranteedCharacters.push(getRandom(upperCasedCharacters));
+  }
+
+  // For loop to iterate over the password length
+  for (var i = 0; i < options.length; i++) {
+    var possibleCharacter = getRandom(possibleCharacters);
+    result.push(possibleCharacter);
+  }
+
+  // Mix in at least one of each guaranteed character in the result
+  for (var i = 0; i < guaranteedCharacters.length; i++) {
+    result[i] = guaranteedCharacters[i];
+  }
+
+  // Transform the result into a string and pass into writePassword
+  return result.join('');
+}
 
 
 // Get references to the #generate element
